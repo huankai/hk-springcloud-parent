e77ad63b54912c887237beb4730fd5f6cc531a3f
huangkai <huankai@139.com>
2018-02-06 23:09:45 +0800

    子

--- hk-discover-eureka/src/main/java/com/hk/discover/eureka/EurekaServerApplication.java
+++ hk-discover-eureka/src/main/java/com/hk/discover/eureka/EurekaServerApplication.java
@@ -8,11 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * <pre>
+ * 	EnableEurekaServer 表示是一个 eureka 服务
+ * </pre>
+ * @author huangkai
  * @author kally
  * @date 2018年2月6日下午5:45:04
  */
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class EurekaServerApplication {
 
 	/**
