3582e1534686249f40801e673291d9cf57596945
huangkai <huankai@139.com>
2018-06-10 22:08:43 +0800

    hystrix

---
+++ hk-hystrix/src/main/java/com/hk/hystrix/controller/TestController.java
@@ -0,0 +1,85 @@
+package com.hk.hystrix.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author huangkai
+ * @date 2018-6-10 11:19
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public static class UserVo {
+
+        private Long id;
+
+        private String userName;
+
+        private String sex;
+
+        private String email;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+    /**
+     * 电影微服务调用用户微服务,
+     * 如果调用用户微服务不成功，会调用 defaultUser  方法
+     *
+     * @param userId
+     */
+    @RequestMapping("user/{userId}")
+    @HystrixCommand(fallbackMethod = "defaultUser")
+    public UserVo getUser(@PathVariable Long userId) {
+        return restTemplate.getForObject("http://user-service/user/" + userId, UserVo.class);
+    }
+
+    /**
+     * @param userId
+     * @return
+     */
+    public UserVo defaultUser(Long userId) {
+        UserVo user = new UserVo();
+        user.setId(userId);
+        user.setUserName("default");
+        return user;
+    }
+}
